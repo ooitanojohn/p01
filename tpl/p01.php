aa1ffcf10a8993f072cc0da31f91c2c38df552d8
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-07-25 21:55:14 +0900

    first commit

---
+++ tpl/p01.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta property="og:title" content="Eliminate Waste">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="p01.php">
+  <meta property="og:image" content="./assets/image/railway-thumbnail.png">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:creator" content="@gurusu_program">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title><?php echo $idea ?></title>
+</head>
+
+<body>
+  <div class="mx-auto container-xl">
+    <header class="container px-0 p-3 fw-bold" id="Top">
+      <div class="d-flex flex-row justify-content-between align-items-center">
+        <a href="#Top"><span class="fs-3">feris nyannyan</span></a>
+        <nav>
+          <a href="#Story" class="col-2 px-0">Story</a>
+          <a href="#Character" class="col-2 px-0">Character</a>
+          <a href="#Works" class="col-2 px-0">Works</a>
+          <a href="#Links" class="col-2 px-0">Links</a>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <article class="vh-100 d-flex flex-row align-items-center">
+        <h1 class="fs-6 fw-normal ms-5 h-25">
+          <span class="d-block fs-3 mb-3">働きたくないです。今日も酒が美味い</span>
+          お金ください、休みください、サボりたいです、不労所得ください。
+        </h1>
+      </article>
+      <article id="Story" class="mb-5">
+        <h2 class="text-center mb-5"><a>Story</a></h2>
+        <div class="ms-5 fw-bold">
+          <p class="mb-0">人が苦手でゲームばかりしていた人間。</p>
+          <p class="mb-0">社会復帰する為に接客業を志すも体力も気力も無く</p>
+          <p>気疲れしてしまい、エンジニアを目指す事に。</p>
+          <p>無駄が多い生き方...それも人生()</p>
+        </div>
+      </article>
+      <article id="Character">
+        <h2 class="text-center mb-5">Character</h2>
+        <section>
+          <h3 class="visually-hidden">自己紹介:性格</h3>
+          <div>
+            <div></div>
+            <div>
+              <p>HAL大阪IT学部WEB開発学科に通う2年生。</p>
+              <p>小さい頃もあまり人の話を聞かなかったらしい</p>
+              <p>最近もあまり聞いていないが聞こうと努力はしているらしい。</p>
+              <p>好きな事は模倣、本、可愛い、音楽。</p>
+              <p>soundcloudでremixされた両曲を探しつつよく聞いている。</p>
+              <p>@tonari_machi_を推している。</p>
+            </div>
+            <div>
+              <p>意識しないとすぐサボりがち</p>
+              <p>年取ってガタがこないような真っ当な人間を目指したい()</p>
+            </div>
+            <div>
+              <p>Qualitification</p>
+              <ul>
+                <li>基本情報技術者 合格</li>
+                <li>日本漢字検定2級 合格</li>
+                <li>日本英語検定2級 合格</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h3 class="visually-hidden">自己紹介:経歴</h3>
+          <div>
+            <div></div>
+            <ul>
+              <li>(1995)&nbsp;&nbsp;生まれる</li>
+              <li>
+                <div>(2011)&nbsp;&nbsp;履正社高校へ</div>
+                <div></div>
+              </li>
+              <li>(2013)&nbsp;&nbsp;つくば開成高校転入(1日4限の人生夏休み開始)</li>
+              <li>(2014) 中部大学入学</li>
+            </ul>
+          </div>
+        </section>
+      </article>
+      <article id="Works">
+        <h2 class="text-center mb-5">Works</h2>
+        <section>
+          <div>
+            <p>to be continued...</p>
+          </div>
+        </section>
+      </article>
+      <article id="Links">
+        <h2 class="text-center mb-5">Links</h2>
+        <section>
+          <h3>自己紹介:サイト、SNSリンク</h3>
+          <ul>
+            <li><a href="https://github.com/ooitanojohn">github</a></li>
+            <li><a href="https://twitter.com/syuto_niimi">twitter</a></li>
+            <li><a href="https://qiita.com/ooitanojohn">Qiita</a></li>
+            <li><a href="http://ooitanojohn.wp.xdomain.jp/">Blog</a></li>
+          </ul>
+        </section>
+      </article>
+    </main>
+    <footer>
+      <small>© 2021 feris nyannyan</small>
+    </footer>
+  </div>
+
+</body>
+
+</html>
